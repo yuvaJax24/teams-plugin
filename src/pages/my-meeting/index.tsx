c7276cbe4769f6ae0aae3d1c4c9b1b245302d91a
yuva <yuva.prasath@flyerssoft.com>
2024-06-06 16:32:53 +0530

    dummy test 2

--- src/pages/my-meeting/index.tsx
+++ src/pages/my-meeting/index.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
-import { useIsAuthenticated } from "@azure/msal-react";
-import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useIsAuthenticated } from "@azure/msal-react";
+// import { useNavigate } from "react-router-dom";
 
 const Meeting = () => {
-  const isSiggedIn = useIsAuthenticated();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!isSiggedIn) {
-      navigate("/login");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSiggedIn]);
+  // const isSiggedIn = useIsAuthenticated();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (!isSiggedIn) {
+  //     navigate("/login");
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [isSiggedIn]);
   return <div>CRB - My Meetings</div>;
 };
 
